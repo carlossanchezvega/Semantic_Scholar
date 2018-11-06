acffe1a3f861f2f13af094b10ad49e4de84d7f15
carlos.sanchez.vega <carlos.sanchez.vega.contractor@bbva.com>
2018-11-06 18:29:05 +0100

    vbxc

--- aux.py
+++ aux.py
@@ -1,6 +1,9 @@
-import dblp
-#do a simple author search for michael ley
-authors = dblp.search('michael ley')
-michael = authors[0]
-print michael.name
-print len(michael.publications)
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
+
+phrases = sent_tokenize(data)
+words = word_tokenize(data)
+
+print(phrases)
+print(words)
